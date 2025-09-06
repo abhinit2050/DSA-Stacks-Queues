4930f2be35f90342d1be61296017c2b37018515f
Abhinit Rai <abhinit1990@gmail.com>
2025-09-06 13:33:20 +0530

    with eval function JS

--- reversePolishNotation.js
+++ reversePolishNotation.js
@@ -1,16 +1,46 @@
+
+// var evalRPN = function(tokens) {
+    
+//     let s=[];
+//     let operands = ["+","-","*","/"]
+
+//     function evaluator(el1, el2, opr){
+//          switch(opr){
+//                 case "+": return  el2 + el1;
+//                 case "-": return  el2 - el1;
+//                 case "*": return el2 * el1;
+//                 case "/": return  Math.trunc(el2 / el1);
+//             }
+//     }
+
+//     for(let i=0;i<tokens.length;i++){
+        
+//         if(operands.includes(tokens[i])){
+//             let temp;
+    
+//             let el1 = s.pop();
+//             let el2 = s.pop();
+            
+//             temp = evaluator(el1,el2,tokens[i]);
+//             s.push(Number(temp))
+//         } else {
+            
+//             s.push(Number(tokens[i]))
+//         }
+//     }
+
+//     return s[0];
+// };
+
+
+
+//
+
+//with eval function
 var evalRPN = function(tokens) {
     
     let s=[];
     let operands = ["+","-","*","/"]
-
-    function evaluator(el1, el2, opr){
-         switch(opr){
-                case "+": return  el2 + el1;
-                case "-": return  el2 - el1;
-                case "*": return el2 * el1;
-                case "/": return  Math.trunc(el2 / el1);
-            }
-    }
 
     for(let i=0;i<tokens.length;i++){
         
@@ -19,11 +49,9 @@ var evalRPN = function(tokens) {
     
             let el1 = s.pop();
             let el2 = s.pop();
-            
-            temp = evaluator(el1,el2,tokens[i]);
-            s.push(Number(temp))
+            temp = eval(`${el2} ${tokens[i]} ${el1}`);   
+            s.push(Math.trunc(Number(temp)))
         } else {
-            
             s.push(Number(tokens[i]))
         }
     }
@@ -32,9 +60,6 @@ var evalRPN = function(tokens) {
 };
 
 
-
-
-
-let tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+let tokens = ["4","-2","/","2","-3","-","-"]
 console.log(evalRPN(tokens));
 
